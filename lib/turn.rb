546d0d9ef25dfecfff4672cc6cb959f84b4094c6
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-03-10 12:14:49 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,8 +19,7 @@ def position_taken(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken(board, index) ? 
-  true
+  if index.between?(0,8) && !position_taken(board, index) ? true : false
   end
 end
 
